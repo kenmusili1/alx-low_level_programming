fe114f76b89680ab0c76757bbe0c23a6001a156d
kenmusili1 <kennedymusili99@gmail.com>
2023-03-16 17:56:51 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line using Betty style
- * Return: 0 (Success)
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-Return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	Return(0);
 }
