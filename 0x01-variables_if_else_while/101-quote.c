fd818bad0649ece473c4daddeea548ddae45ccf9
kenmusili1 <kennedymusili99@gmail.com>
2023-03-20 19:57:21 -0700

    Quote

--- 0x01-variables_if_else_while/101-quote.c
+++ 0x01-variables_if_else_while/101-quote.c
@@ -4,10 +4,9 @@
 /**
   * main - Entry point
   *
-  * return: Always 1 (failure)
+  * Return: Always 1 (failure)
   */
 int main(void)
-
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
