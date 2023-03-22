0432adde705ee9a05a912d9e70f1bdff68f21696
kenmusili1 <kennedymusili99@gmail.com>
2023-03-21 18:58:19 -0700

    Print Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *print_alphabet_x10- print alphabet ten times
+  *
+  *Return: Always 0 (Success)
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 1; i++)
+	{
+		for (j = 7; j <= 12; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+
+}
