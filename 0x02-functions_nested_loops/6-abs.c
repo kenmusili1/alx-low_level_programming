20d254e783c82850055b3f71ea5a0ed4a03d37d9
kenmusili1 <kennedymusili99@gmail.com>
2023-03-22 17:17:48 -0700

    ABS

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,19 +2,15 @@
 /**
   *_abs - Computes tha absolute value
   *of an integer
-  *@i: input  num as integer
+  *@n: input  num as integer
   *
-  *Return: abaolute value
+  *Return: absolute value of n
   */
-int _abs(int i)
+int _abs(int n)
 {
-	if (i >= 0)
-	{
-		return (i);
-	}
-	else
-	{
-		return (i * -i);
-	}
-
+if (n < 0)
+return (-n);
+else
+return (n);
 }
+
