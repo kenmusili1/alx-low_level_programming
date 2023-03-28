3e789d5e3e84dc6ed8400458e9804bd776e16678
kenmusili1 <kennedymusili99@gmail.com>
2023-03-28 15:18:33 -0700

    Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string, new line
+ *
+ * @str: pointer to the string to be printed
+ */
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
