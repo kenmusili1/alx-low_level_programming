1829972beebed59510755fbc193db5adb6fb7993
kenmusili1 <kennedymusili99@gmail.com>
2023-04-05 15:54:23 -0700

    Sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the square root of n, or -1 if n does not have a natural square root
+ */
+int _sqrt_helper(int n, int i);
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+
+return (_sqrt_helper(n, 1));
+}
+
+int _sqrt_helper(int n, int i)
+{
+if (i * i == n)
+{
+return (i);
+}
+else if (i * i > n)
+{
+return (-1);
+}
+else
+{
+return (_sqrt_helper(n, i + 1));
+}
+}
