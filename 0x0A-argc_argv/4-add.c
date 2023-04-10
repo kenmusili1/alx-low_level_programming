df05f617fc94842003c7d7736b1e767bb27a612f
kenmusili1 <kennedymusili99@gmail.com>
2023-04-10 16:07:08 -0700

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -28,10 +28,10 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		
+
 		sum += atoi(argv[i]);
 	}
-	
+
 	printf("%d\n", sum);
 	return (0);
 }
